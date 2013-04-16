9a86f93fa7fed0ba2d55ee22ac4ef6dd994a8ae9
MiguelVis <miguelvisgarcia@gmail.com>
2013-04-17 00:18:24 +0200

    Añadido nº de versión y código para imprimir mensaje en la consola.

--- HelloWorld/src/HelloToTheWorld.java
+++ HelloWorld/src/HelloToTheWorld.java
@@ -4,6 +4,7 @@
 
 /**
  * @author MiguelVis
+ * @version 1.0 - Abril 2013
  *
  */
 public class HelloToTheWorld {
@@ -12,7 +13,7 @@ public class HelloToTheWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Hello, World");
 
 	}
 
