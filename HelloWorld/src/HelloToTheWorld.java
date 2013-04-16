082168dc2e39c00ff88f1b0cb06260f402c37288
MiguelVis <miguelvisgarcia@gmail.com>
2013-04-17 00:26:44 +0200

    Añadida descripción del programa para Javadoc y el carácter '!' en el mensaje de salida.

--- HelloWorld/src/HelloToTheWorld.java
+++ HelloWorld/src/HelloToTheWorld.java
@@ -6,6 +6,8 @@
  * @author MiguelVis
  * @version 1.0 - Abril 2013
  *
+ * El archiconocido programa que muestra "Hello World!" en
+ * la consola. Esta vez, en Java.
  */
 public class HelloToTheWorld {
 
@@ -13,7 +15,7 @@ public class HelloToTheWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
 
 	}
 
